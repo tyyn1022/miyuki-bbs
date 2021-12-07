97b237ca3fa694dc84d26071a650e4232854e63a
tyyn1022 <tyyn1022@gmail.com>
2021-12-07 19:31:28 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"miyuki/tool"
+)
+
+func main()  {
+	cfg, err := tool.ParseConfig("./config/app.json")
+	if err != nil {
+		panic(err)
+	}
+	r := gin.Default()
+
+	r.Run(cfg)
+}
